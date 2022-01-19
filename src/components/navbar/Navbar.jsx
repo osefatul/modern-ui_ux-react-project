3208ad08b46d23dd1ce1290ad228c38cef76f443
osefatul <sefaullahomar2@gmail.com>
2022-01-18 22:35:39 -0800

    WIP navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 import "./navbar.css";
+import logo from "../../assets/logo.svg";
 function Navbar() {
-  return <div>navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbarLinks">
+        <div className="navbarLinksLogo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="navbarLinksContainer">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is GPT3?</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
