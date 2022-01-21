5e5a2330cdc5d2ae6d09906e87e0e37800ac39c3
osefatul <sefaullahomar2@gmail.com>
2022-01-21 01:55:05 -0800

    done with navbar

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Website to use for [gradient generator](https://angrytools.com/gradient/)
 Website used for [animation](https://animista.net/)
+
+### Navabar
+
+- We have used navbarMenu and toggleMenu in order to determine our app work for mobile page as well.
+- using > in css means directly under the parent class name. The css change will affect only when the child classname in under the parent classname at that time. it doesn't bring changes if the child is a refrenced component. in that case to bring changed in to the child component we have to use space:
+  .parentClassName childClassname/htmlTag.
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,33 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import logo from "../../assets/logo.svg";
-function Navbar() {
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+
+const Menu = () => (
+  <>
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#wgpt3">What is GPT3?</a>
+    </p>
+    <p>
+      <a href="#possibility">Open AI</a>
+    </p>
+    <p>
+      <a href="#features">Case Studies</a>
+    </p>
+    <p>
+      <a href="#blog">Library</a>
+    </p>
+  </>
+);
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="navbar">
       <div className="navbarLinks">
         <div className="navbarLinksLogo">
-          <img src={logo} alt="" />
+          <img src={logo} />
         </div>
         <div className="navbarLinksContainer">
-          <p>
-            <a href="#home">Home</a>
-          </p>
-          <p>
-            <a href="#wgpt3">What is GPT3?</a>
-          </p>
-          <p>
-            <a href="#possibility">Open AI</a>
-          </p>
-          <p>
-            <a href="#features">Case Studies</a>
-          </p>
-          <p>
-            <a href="#blog">Library</a>
-          </p>
+          <Menu />
         </div>
+      </div>
+      <div className="navbarSign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="navbarMenu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="navbarMenuContainer scale-up-center">
+            <div className="navbarMenuContainerLinks">
+              <Menu />
+            </div>
+            <div className="navbarMenuContainerLinksSign">
+              <p>Sign in</p>
+              <button type="button">Sign up</button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
