16a35601952e8e30ff4662fb5838d9dff5c96f22
osefatul <sefaullahomar2@gmail.com>
2022-01-23 09:58:57 -0800

    done with cta

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./cta.css";
 function Cta() {
-  return <div></div>;
+  return (
+    <div className="cta">
+      <div className="ctaContent">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="ctaBtn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 }
 
 export default Cta;
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import "./possibility.css";
+import possibilityImage from "../../assets/possibility.png";
 function Possibility() {
-  return <div>possibility</div>;
+  return (
+    <div className="possibility section__padding" id="possibility">
+      <div className="possibilityImage">
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className="possibilityContent">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">
+          The possibilities are <br /> beyond your imagination
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  );
 }
 
 export default Possibility;
